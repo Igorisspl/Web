98d0e93d90900cbf3a87fcad878330d12b8c97af
Igorisspl <S00238949@atu.ie>
2024-09-30 16:30:45 +0100

    worksheet 3

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 
+
 import express, {Application, Request, Response} from "express" ;
 
 import morgan from "morgan";
 
 
 import userRoutes from './routes/users';
+import {authenticateKey} from './middleware/auth.middleware';
 
 const PORT = process.env.PORT || 3000;
 
@@ -14,6 +16,8 @@ const app: Application = express();
 
 app.use(morgan("tiny"));
 app.use(express.json());
+
+//app.use(authenticateKey); - all the routes below
 
 app.get("/ping", async (_req : Request, res: Response) => {
     res.json({
@@ -25,7 +29,7 @@ app.get("/ping", async (_req : Request, res: Response) => {
 app.get('/bananas', async (_req : Request, res: Response) =>
   res.send('hello world, this is bananas - ha ha ha ha hda ha '));
 
-  app.use('/api/v1/users', userRoutes)
+app.use('/api/v1/users', authenticateKey, userRoutes)
 
 
 app.listen(PORT, () => {
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -7,11 +7,13 @@ import {
   deleteUser,
 } from '../controllers/users';
 
+import { authenticateKey } from '../middleware/auth.middleware';
+
 const router: Router = express.Router();
 
 router.get('/', getUsers);
-router.get('/:id', getUserById);
-router.post('/', createUser);
+router.get('/:id',  getUserById);
+router.post('/', authenticateKey, createUser);
 router.put('/:id', updateUser);
 router.delete('/:id', deleteUser);
 
